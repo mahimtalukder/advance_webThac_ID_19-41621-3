0e1e4332dccc6c016bf331449a571a583f5dee2b
mahimtalukder <19-41621-3@student.aiub.edu>
2022-06-08 18:40:48 +0600

    Task 3 created

---
+++ APWT_TASK_3/routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PagesController;
+use App\Http\Controllers\InformationController;
+use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\ContactController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+/*Route::get('/', function () {
+    return view('welcome');
+});*/
+
+Route::get('/',[PagesController::class, 'home'])->name("home");
+Route::get('/aboutUs',[PagesController::class, 'aboutUs'])->name("aboutUs");
+
+
+Route::get('/teams',[InformationController::class, 'teams'])->name("teams");
+Route::get('/service',[InformationController::class, 'service'])->name("service");
+
+Route::get('/employeeSignIn',[EmployeeController::class, 'employeeSignIn'])->name("employeeSignIn");
+Route::post('/employeeSignIn',[EmployeeController::class, 'employeeSignInSubmitted'])->name("employeeSignInSubmitted");
+
+Route::get('/employeeCreate',[EmployeeController::class, 'employeeCreate'])->name("employeeCreate");
+Route::post('/employeeCreate',[EmployeeController::class, 'employeeCreateSubmitted'])->name('employeeCreate');
+Route::get('/employeeCreateSuccess',[EmployeeController::class, 'employeeCreateSuccess'])->name("employeeCreateSuccess");
+
+Route::get('/contact',[ContactController::class, 'contact'])->name("contact");
+Route::post('/contact',[ContactController::class, 'contactMessageSubmitted'])->name("contactMessageSubmitted");
